7aafa59838a003ffb49b7443d472bf3d421d26dc
Robin <robinthomasp@gmx.com>
2019-09-10 13:24:43 +0530

    Eureka server

---
+++ 1.Eureka server/PhotoAppApiUsers/src/main/java/com/microservice/photoapp/api/users/ui/contoller/UsersController.java
@@ -0,0 +1,17 @@
+package com.microservice.photoapp.api.users.ui.contoller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+	
+	@GetMapping("/status/check")
+	public String status(){
+		return"working";
+	}
+
+	
+}
